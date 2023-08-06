91cc6a2f5ae19bf566468642bdf991401990e585
Phil Mills <philliprmills@gmail.com>
2023-08-06 14:52:52 -0400

    order results by id by default, fix bug with pagination offset and limit values

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -9,7 +9,7 @@ class SubscribersController < ApplicationController
     subscribers = Subscriber.all
 
     total_records = subscribers.count
-    limited_subscribers = subscribers[offset..limit]
+    limited_subscribers = subscribers[offset...(offset+limit)]
 
     render json: {subscribers: limited_subscribers, pagination: pagination(total_records)}, formats: :json
   end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,5 @@
 class Subscriber < ApplicationRecord
+    default_scope { order(id: :asc) }
     validates :name, presence: true
     validates :email, presence: true, email: true
     validates_uniqueness_of :email, :case_sensitive => false
